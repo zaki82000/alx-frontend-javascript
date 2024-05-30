c823e4cf1215c4fc4ea0b51f74c0fda24d395f1e
zaki82000 <yabdelshafy0@gmail.com>
2024-05-30 04:40:22 +0300

    add promose first java file

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+
+    if (success) {
+        resolve('success')
+    } else {
+        reject(new Error('failed'));
+    }
+  });
+}
