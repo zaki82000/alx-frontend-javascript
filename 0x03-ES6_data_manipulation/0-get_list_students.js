7ccd3fb0bb6a75e33e2c417768b823cd4cdfe704
zaki82000 <yabdelshafy0@gmail.com>
2024-06-04 23:24:00 +0300

    add 0-get_list_students js file

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
-const getListStudents = () => ({
-  students: [
-    {id: 1, firstName: "Guillaume", location: "San Francisco"},
-    {id: 2, firstName: "James", location: "Columbia"},
-    {id: 3, firstName: "Serena", location: "San Francisco"}
-  ]
-});
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
