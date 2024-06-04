1c08b88a417e035ad43a5e180802359858dec4e5
zaki82000 <yabdelshafy0@gmail.com>
2024-06-05 02:36:39 +0300

    add get_students_by_loc js file

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(arr) {
-  return arr.filter((student) => student.location === 'San Francisco');
+export default function getStudentsByLocation(arr, city) {
+  return arr.filter((student) => student.location === city);
 }
