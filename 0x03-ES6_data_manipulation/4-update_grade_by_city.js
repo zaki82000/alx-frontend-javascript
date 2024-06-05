2c1887e1ebc7085dacf56139acd28012eb03f6fa
zaki82000 <yabdelshafy0@gmail.com>
2024-06-05 04:09:52 +0300

    add update_grade_by_city js file

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newGrade = newGrades.find(
+        (grade) => grade.studentId === student.id,
+      );
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
+}
